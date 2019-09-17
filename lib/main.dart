a70c874fdc7314666edd60877463c4b95bfa7ad8
Halldór Logi Hilmarsson <halldorlogi@gmail.com>
2019-09-17 19:48:43 +0000

    Final

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,51 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask me anything'),
+          backgroundColor: Colors.blueAccent.shade700,
+        ),
+        body: EightBall(),
+      ),
+    ),
+  );
+}
+
+class EightBall extends StatefulWidget {
+  @override
+  _EightBallState createState() => _EightBallState();
+}
+
+class _EightBallState extends State<EightBall> {
+  int ballNumber = 1;
+
+  void setBallNumber() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            //flex: 2,
+            child: FlatButton(
+              onPressed: () {
+                setBallNumber();
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
